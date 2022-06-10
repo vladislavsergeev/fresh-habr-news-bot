3b6d43ffcf7f9185fcc81d8e0fb562fc7f31403b
Vladislav Sergeev <vssx04@gmail.com>
2022-06-10 11:40:06 +0700

    bot files upload

---
+++ main.py
@@ -0,0 +1,91 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+fresh_news = {}
+news_dict = {}
+
+
+def get_first_news():
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/102.0.5005.63 Safari/537.36 "
+    }
+
+    url = "https://habr.com/ru/flows/admin/"
+    r = requests.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    articles_cards = soup.find_all("article", class_="tm-articles-list__item")
+
+    for article in articles_cards:
+        article_title = article.find("h2", class_="tm-article-snippet__title tm-article-snippet__title_h2").text.strip()
+        article_url = f'https://habr.com{article.find("a", class_="tm-article-snippet__title-link").get("href")}'
+
+        article_date_time = article.find("time").get("title")
+
+        article_id = article_url.split("/")[-2]
+
+        news_dict[article_id] = {
+            "article_date_time": article_date_time,
+            "article_title": article_title,
+            "article_url": article_url
+        }
+
+    with open("news_dict.json", "w") as file:
+        json.dump(news_dict, file, indent=4, ensure_ascii=False)
+
+
+def check_news_update():
+    with open("news_dict.json") as file:
+        news_dict = json.load(file)
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/102.0.5005.63 Safari/537.36 "
+    }
+
+    url = "https://habr.com/ru/flows/admin/"
+    r = requests.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    articles_cards = soup.find_all("article", class_="tm-articles-list__item")
+
+    for article in articles_cards:
+        article_url = f'https://habr.com{article.find("a", class_="tm-article-snippet__title-link").get("href")}'
+        article_id = article_url.split("/")[-2]
+
+        if article_id in news_dict:
+            continue
+        else:
+            article_title = article.find("h2",
+                                         class_="tm-article-snippet__title tm-article-snippet__title_h2").text.strip()
+
+            article_date_time = article.find("time").get("title")
+
+            news_dict[article_id] = {
+                "article_date_time": article_date_time,
+                "article_title": article_title,
+                "article_url": article_url,
+
+            }
+
+            fresh_news[article_id] = {
+                "article_date_time": article_date_time,
+                "article_title": article_title,
+                "article_url": article_url,
+
+            }
+
+    with open("news_dict.json", "w") as file:
+        json.dump(news_dict, file, indent=4, ensure_ascii=False)
+
+    return fresh_news
+
+
+def main():
+    get_first_news()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tg_bot.py
@@ -0,0 +1,104 @@
+import json
+
+import logging
+
+import os
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram.utils.markdown import hbold, hlink
+from aiogram.dispatcher.filters import Text
+
+from main import check_news_update
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# Настройки WebHook
+
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# Настройки веб-сервера
+
+WEBAPP_HOST = '0.0.0.0'
+
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup():
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown():
+    await bot.delete_webhook()
+
+
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    start_buttons = ["Все новости", "Последние пять новостей", "Свежие новости"]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_buttons)
+
+    await message.answer("Лента новостей", reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals="Все новости"))
+async def get_all_news(message: types.Message):
+    with open("news_dict.json") as file:
+        news_dict = json.load(file)
+
+    for k, v in sorted(news_dict.items()):
+        news = f"{hbold(v['article_date_time'])}\n\n" \
+               f"{hlink(v['article_title'], v['article_url'])}\n\n"
+
+        await message.answer(news)
+
+
+@dp.message_handler(Text(equals="Последние пять новостей"))
+async def get_last_five_news(message: types.Message):
+    with open("news_dict.json") as file:
+        news_dict = json.load(file)
+
+    for k, v in sorted(news_dict.items())[-5:]:
+        news = f"{hbold(v['article_date_time'])}\n\n" \
+               f"{hlink(v['article_title'], v['article_url'])}\n\n"
+
+        await message.answer(news)
+
+
+@dp.message_handler(Text(equals="Свежие новости"))
+async def get_fresh_news(message: types.Message):
+    fresh_news = check_news_update()
+
+    if len(fresh_news) >= 1:
+        for k, v in sorted(fresh_news.items()):
+            news = f"{hbold(v['article_date_time'])}\n\n" \
+                   f"{hlink(v['article_title'], v['article_url'])}\n\n"
+
+            await message.answer(news)
+
+    else:
+        await message.answer("Свежих новостей пока нет")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
